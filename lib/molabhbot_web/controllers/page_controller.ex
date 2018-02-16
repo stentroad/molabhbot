177156b25794084bd78ae960746c3a6f792df42e
Brian Stubbs <stentroad@gmail.com>
2018-02-16 15:37:27 -0200

    initial login form

--- lib/molabhbot_web/controllers/page_controller.ex
+++ lib/molabhbot_web/controllers/page_controller.ex
@@ -1,7 +1,13 @@
 defmodule MolabhbotWeb.PageController do
   use MolabhbotWeb, :controller
+  alias Molabhbot.Accounts
 
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def login(conn, _params) do
+    render conn, "login.html", changeset: Accounts.change_user(%Accounts.User{})
+  end
+
 end
--- lib/molabhbot_web/router.ex
+++ lib/molabhbot_web/router.ex
@@ -17,6 +17,7 @@ defmodule MolabhbotWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/login", PageController, :login
     resources "/users", UserController
     resources "/roles", RoleController
   end
