d56f3e362bd91998b60db15147175a17e9e40201
Brian Stubbs <stentroad@gmail.com>
2018-02-18 16:45:33 -0300

    basic elm config

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,10 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+import Elm from "./elm.js"
+
+let counterDiv = document.getElementById("elm-app")
+if (counterDiv !== undefined) {
+    Elm.Main.embed(counterDiv)
+}
