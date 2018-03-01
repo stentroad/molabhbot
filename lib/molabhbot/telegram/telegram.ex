ab0afd051ce9cdc59e2f2d7b4d2c29fd4224344c
Brian Stubbs <stentroad@gmail.com>
2018-02-28 23:42:40 -0300

    tidy up unneeded params

--- lib/molabhbot/telegram/telegram.ex
+++ lib/molabhbot/telegram/telegram.ex
@@ -3,8 +3,8 @@ defmodule Molabhbot.Telegram do
   alias Molabhbot.Telegram.Arduino
   alias Molabhbot.Telegram.Message
 
-  def handle_new_message(%{"message" => msg} = params), do: process_message(msg)
-  def handle_new_message(%{"edited_message" => msg} = params), do: process_message(msg)
+  def handle_new_message(%{"message" => msg}), do: process_message(msg)
+  def handle_new_message(%{"edited_message" => msg}), do: process_message(msg)
   def handle_new_message(%{"inline_query" => _} = params), do: process_inline_query(params)
   def handle_new_message(%{"callback_query" => _} = params), do: process_callback_query(params)
 
