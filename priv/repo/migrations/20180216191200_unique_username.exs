ea3e742daa01c65f5e08038afc35bf08b5fb269c
Brian Stubbs <stentroad@gmail.com>
2018-05-03 16:27:04 -0300

    remove old migrations

--- priv/repo/migrations/20180216191200_unique_username.exs
+++
@@ -1,8 +0,0 @@
-defmodule Molabhbot.Repo.Migrations.UniqueUsername do
-  use Ecto.Migration
-
-  def change do
-    create unique_index(:users, [:username])
-    drop unique_index(:users, [:email])
-  end
-end
