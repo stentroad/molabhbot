9b3841e79a4b5c2b2bf7e8fe88b99972fad447aa
Brian Stubbs <stentroad@gmail.com>
2018-05-03 18:52:44 -0300

    router updated to use coherence

--- lib/molabhbot_web/router.ex
+++ lib/molabhbot_web/router.ex
@@ -20,6 +20,14 @@ defmodule MolabhbotWeb.Router do
     plug Coherence.Authentication.Session, protected: true
   end
 
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  pipeline :check_telegram_key do
+    plug :validate_telegram_key
+  end
+
   scope "/" do
     pipe_through :browser
     coherence_routes()
@@ -33,92 +41,33 @@ defmodule MolabhbotWeb.Router do
   scope "/", MolabhbotWeb do
     pipe_through :browser
 
-    get "/", PageController, :index
     # add public resources below
   end
 
-  # scope "/", MyProjectWeb do
-  #   pipe_through :protected
+  scope "/", MolabhbotWeb do
+    pipe_through :protected
+    # add protected resources below
+    get "/", PageController, :index
+  end
 
-  #   # add protected resources below
-  #   resources "/admin", MyProjectWeb.PrivateController
-  # end
+  scope "/telegram/:telegram_key", MolabhbotWeb do
+    pipe_through :api
+    pipe_through :check_telegram_key
 
-  # old auth stuff below here
+    post "/", TelegramController, :new_message
+  end
 
-  # pipeline :authenticated do
-  #   plug :ensure_authenticated
-  # end
-
-  # pipeline :api do
-  #   plug :accepts, ["json"]
-  #   plug :fetch_session
-  # end
-
-  # pipeline :check_telegram_key do
-  #   plug :validate_telegram_key
-  # end
-
-  # scope "/login", MolabhbotWeb do
-  #   pipe_through :browser
-
-  #   get "/", PageController, :show_login
-  #   post "/", PageController, :login
-  # end
-
-  # scope "/logout", MolabhbotWeb do
-  #   pipe_through :browser
-
-  #   get "/", PageController, :logout
-  # end
-
-  # scope "/", MolabhbotWeb do
-  #   pipe_through :browser # Use the default browser stack
-  #   pipe_through :authenticated
-
-  #   get "/", PageController, :index
-  #   resources "/roles", RoleController
-  # end
-
-  # # Other scopes may use custom stacks.
-  # scope "/api", MolabhbotWeb do
-  #   pipe_through :api
-  #   pipe_through :authenticated
-
-  #   resources "/users", UserController
-  # end
-
-  # scope "/telegram/:telegram_key", MolabhbotWeb do
-  #   pipe_through :api
-  #   pipe_through :check_telegram_key
-
-  #   post "/", TelegramController, :new_message
-  # end
-
-  # defp validate_telegram_key(conn,_) do
-  #   telegram_webhook_key = MolabhbotWeb.Endpoint.config(:secret_key_telegram)
-  #   sent_key = conn.path_params["telegram_key"]
-  #   if sent_key == telegram_webhook_key do
-	# conn 
-  #   else
-  #       conn |> put_resp_content_type("text/plain")
-  #            |> send_resp(401, "unauthorized") 
-  #            |> halt()
-  #   end
-  # end
-
-  # defp ensure_authenticated(conn,_) do
-  #   user_id = Plug.Conn.get_session(conn, :user_id)
-  #   IO.inspect user_id, label: "logged in user_id"
-  #   if user_id do
-  #     conn
-  #     |> assign(:user, Molabhbot.Accounts.get_user!(user_id))
-  #     |> configure_session(renew: true)
-  #   else
-  #     conn
-  #     |> assign(:user, nil)
-  #     |> redirect(to: "/login")
-  #   end
-  # end
+  defp validate_telegram_key(conn,_) do
+    telegram_webhook_key = MolabhbotWeb.Endpoint.config(:secret_key_telegram)
+    sent_key = conn.path_params["telegram_key"]
+    if sent_key == telegram_webhook_key do
+	    conn
+    else
+      conn
+      |> put_resp_content_type("text/plain")
+      |> send_resp(401, "unauthorized")
+      |> halt()
+    end
+  end
 
 end
