bf867805a911209c96532ddf116a36d10f306276
Brian Stubbs <stentroad@gmail.com>
2018-05-03 16:47:39 -0300

    set up initial test user

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,9 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+# as suggested by coherence installation script
+Molabhbot.Repo.delete_all Molabhbot.Coherence.User
+
+Molabhbot.Coherence.User.changeset(%Molabhbot.Coherence.User{}, %{name: "Test User", email: "testuser@example.com", password: "secret", password_confirmation: "secret"})
+|> Molabhbot.Repo.insert!
