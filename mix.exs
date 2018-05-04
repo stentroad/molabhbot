db45317e68db7d2aaf6f4886cc0f099728bd1bd7
Brian Stubbs <stentroad@gmail.com>
2018-05-04 18:04:26 -0300

    run the seeds when testing so testuser is created

--- mix.exs
+++ mix.exs
@@ -64,7 +64,7 @@ defmodule Molabhbot.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+      "test": ["ecto.create --quiet", "ecto.migrate",  "run priv/repo/seeds.exs", "test"]
     ]
   end
 end
