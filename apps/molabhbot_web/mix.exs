c9c21bde73300fc23412459de7c9fff5173b78ad
Brian Stubbs <stentroad@gmail.com>
2018-07-31 13:53:30 -0300

    molabhbot_web mix.exs .gitignore README.md

---
+++ apps/molabhbot_web/mix.exs
@@ -0,0 +1,69 @@
+defmodule Molabhbot.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :molabhbot_web,
+      version: "0.0.1",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.4",
+      elixirc_paths: elixirc_paths(Mix.env),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers,
+      start_permanent: Mix.env == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
+  end
+
+  # Configuration for the OTP application.
+  #
+  # Type `mix help compile.app` for more information.
+  def application do
+    [
+      mod: {MolabhbotWeb.Application, []},
+      extra_applications: [:logger, :runtime_tools, :timex, :coherence]
+    ]
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
+
+  # Specifies your project dependencies.
+  #
+  # Type `mix help deps` for examples and options.
+  defp deps do
+    [
+      {:phoenix, "~> 1.3.3"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_ecto, "~> 3.2"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_html, "~> 2.10"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.13.1"},
+      {:cowboy, "~> 1.0"},
+      {:bcrypt_elixir, "~> 1.0"},
+      {:httpoison, "~> 1.0"},
+      {:timex, "~> 3.2"},
+      {:gen_state_machine, "~> 2.0"},
+      {:gproc, "~> 0.6.1"},
+      {:fuzzyurl, "~> 0.9.1"},
+      {:floki, "~> 0.20.0"},
+      {:exsync, "~> 0.2.1", only: :dev},
+      {:quantum, "~> 2.2"},
+      {:coherence, "~> 0.5.0"},
+      {:credo, "~> 0.9.2", only: [:dev, :test]}
+    ]
+  end
+
+  # Aliases are shortcuts or tasks specific to the current project.
+  # For example, we extend the test task to create and migrate the database.
+  #
+  # See the documentation for `Mix` for more info on aliases.
+  defp aliases do
+    ["test": ["ecto.create --quiet", "ecto.migrate", "test"]]
+  end
+end
