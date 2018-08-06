89a5c3d02bb3ea8035be8d3dfa7d95083353eef4
Brian Stubbs <stentroad@gmail.com>
2018-08-06 12:00:42 -0300

    debug failing posts

--- apps/molabhbot/lib/molabhbot/telegram/reply.ex
+++ apps/molabhbot/lib/molabhbot/telegram/reply.ex
@@ -2,14 +2,14 @@ defmodule Molabhbot.Telegram.Reply do
 
   def post_reply(reply, endpoint) do
     api_token = MolabhbotWeb.Endpoint.config(:telegram_api_token)
-    post_result = HTTPoison.post!(
+    post_result = HTTPoison.post(
       "https://api.telegram.org/bot#{api_token}/#{endpoint}",
       Poison.encode!(
-        IO.inspect reply, label: "posting:"
+        IO.inspect reply, label: "posting"
       ),
       [{"Content-type", "application/json"}]
     )
-    IO.inspect post_result, label: "post result:"
+    IO.inspect post_result, label: "post result"
   end
 
 end
